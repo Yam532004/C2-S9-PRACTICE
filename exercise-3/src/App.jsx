82ad51f52f32b8073763e1af7c01f3be4bf5a2de
Yam532004 <yam532004@gmail.com>
2024-06-08 10:58:15 +0700

    ex3

--- exercise-3/src/App.jsx
+++ exercise-3/src/App.jsx
@@ -1,25 +1,39 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
   /* You will need to use many state to keep the inut values and other needs */
-
+  const [A, setA] = useState();
+  const [B, setB] = useState();
+  const [result, setResult] = useState();
   /* You will need some function to handle the key pressed and button events */
-
+function onA(ev) {
+  setA(+ev.target.value);
+}
+function onB(ev) {
+  setB(+ev.target.value);
+}
+function Calculator() {
+  /* You will need a function to calculate the sum of the 2 numbers */
+  setResult( A + B);
+  /* You will need a function to handle the error message */
+  
+  /* You will need a function to display the result */
+}
   return (
     <main>
       <h1>Calculator</h1>
 
       <label>A =</label>
-      <input onKeyUp={onA} />
-
+      <input onKeyUp={onA} type="number" />
       <label>B =</label>
-      <input onKeyUp={onB} />
+      <input onKeyUp={onB} type="number" />
 
       <label>A + B =</label>
 
       {/* When Compute buton is clicked, this input display the sum of the 2 numbers, or the error message in RED */}
-      <input disabled />
-      <button>Compute</button>
+      <input disabled value={result} />
+      <button onClick={Calculator}>Compute</button>
     </main>
   );
 }
